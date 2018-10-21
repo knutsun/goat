35c0ef58a8eacec05c90717439dfbecaad55e79a
Chaz Selph <chaz@chaz.local>
2018-10-21 14:56:05 -0600

    new URL + view for adding to existing lists - FT passes

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -28,9 +28,45 @@ class NewVisitorTest(LiveServerTestCase):
 	def tearDown(self):
 		self.browser.quit()
 
-	def test_can_start_a_list_and_retrieve_it_later(self):
-		#Edith has heard about a cool new online to-do app. she goes
-		#to check out its homepage
+	# def test_can_start_a_list_and_retrieve_it_later(self):
+	# 	#Edith has heard about a cool new online to-do app. she goes
+	# 	#to check out its homepage
+	# 	self.browser.get(self.live_server_url)
+
+	# 	#She notices the page title and header mention to-do lists
+	# 	self.assertIn('To-Do', self.browser.title)
+	# 	header_text = self.browser.find_element_by_tag_name('h1').text
+	# 	self.assertIn('To-Do', header_text)
+		
+	# 	#She is invited to enter a to-do item
+	# 	inputbox = self.browser.find_element_by_id('id_new_item')
+	# 	self.assertEqual(
+	# 		inputbox.get_attribute('placeholder'),
+	# 		'Enter a to-do item'
+	# 	)
+
+	# 	#She types "Buy peacock feathers" into a text box
+	# 	inputbox.send_keys('Buy peacock feathers')
+
+	# 	#When she hits enter, the page updates, and now the page lists
+	# 	# "#1: Buy peacock feathers" as an item in a to-do list table
+	# 	inputbox.send_keys(Keys.ENTER)
+	# 	self.wait_for_row_in_list_table('1: Buy peacock feathers')
+
+	# 	#There is still a text box inviting her to add another item
+	# 	#She enters "Use peacock feathers to make a fly"
+	# 	inputbox = self.browser.find_element_by_id('id_new_item')
+	# 	inputbox.send_keys('Use peacock feathers to make a fly')
+	# 	inputbox.send_keys(Keys.ENTER)
+
+	# 	#The page updates again, and now shows both items on her list
+	# 	self.wait_for_row_in_list_table('1: Buy peacock feathers')
+	# 	self.wait_for_row_in_list_table('2: Use peacock feathers to make a fly')
+
+	# 	#Satisfied, she goes back to sleep
+
+	def test_can_start_a_list_for_one_user(self):
+		#Edith has heard about a cool new online to-do app. She goes...
 		self.browser.get(self.live_server_url)
 
 		#She notices the page title and header mention to-do lists
@@ -60,8 +96,8 @@ class NewVisitorTest(LiveServerTestCase):
 		inputbox.send_keys(Keys.ENTER)
 
 		#The page updates again, and now shows both items on her list
-		self.wait_for_row_in_list_table('1: Buy peacock feathers')
 		self.wait_for_row_in_list_table('2: Use peacock feathers to make a fly')
+		self.wait_for_row_in_list_table('1: Buy peacock feathers')
 
 		#Satisfied, she goes back to sleep
 
